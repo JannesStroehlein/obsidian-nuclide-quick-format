f96042bda28da2e28fe6c75505f4685710848f7b
Jannes Ströhlein <48127946+JannesStroehlein@users.noreply.github.com>
2024-01-23 11:19:13 +0100

    work on editor extension

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
+import { group } from 'console';
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Annotation, EditorState, Extension, StateField, Transaction, TransactionSpec } from '@codemirror/state';
 
 // Remember to rename these classes and interfaces!
+
+const ProgramTxn = Annotation.define<boolean>();
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -16,51 +20,18 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerEditorExtension(EditorState.transactionFilter.of(this.convertFilter));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'quick-format-selected-nuclide',
+			name: 'Quick format selected nuclide',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				let match = editor.getSelection().match("(\\d{1,3})_(\\d{1,3})([A-z]{1,3})");
+				console.log(match);
+				if (match && match.length == 4){
+					editor.replaceSelection(`$^{${match[1]}}_{${match[2]}}${match[3]}$`)
 				}
 			}
 		});
@@ -74,6 +45,7 @@ export default class MyPlugin extends Plugin {
 			console.log('click', evt);
 		});
 
+		
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
@@ -89,22 +61,23 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
+	convertFilter = (tr: Transaction): TransactionSpec | readonly TransactionSpec[] => {
+		if (!tr.docChanged || tr.annotation(ProgramTxn)) { return tr; }
+		let shouldHijack = true; // Hijack when some rules match all changes
+		const changes: TransactionSpec[] = [];
+		tr.changes.iterChanges((fromA, toA, fromB, toB, inserted) => {
+			if (!shouldHijack) { return; }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+			console.log(fromA, toA, fromB, toB, inserted);
+			if (shouldHijack) { tr = tr.startState.update(...changes); }
+			
+			
+		});
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+		if (shouldHijack) { tr = tr.startState.update(...changes); }
+		return tr;
+	};
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
